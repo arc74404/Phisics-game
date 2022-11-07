6fb3a8de01174f97c390e9e2ac7f2bad916d5f94
ARS.CPP <108454901+arc74404@users.noreply.github.com>
2022-11-07 18:52:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@ int main()
     sf::RenderWindow window(sf::VideoMode(700, 700), "SFML works!");
     
     Object cube(constParametrs(10, 50, 50));
-    cube.setPosition(sf::Vector2f(350, 100));
+    cube.shape.setPosition(sf::Vector2f(350, 100));
 
     Object floor(constParametrs(1000000, 700, 30));
-    floor.setPosition(sf::Vector2f(0, 600));
+    floor.shape.setPosition(sf::Vector2f(0, 600));
 
+    current cubres = cube.get_resultant();
+    cubres.vector_coordinates.x /= 300;
+    cubres.vector_coordinates.y /= 300;
 
     while (window.isOpen())
     {
         sf::Event event;
+        window.clear();
+
+        cube.move(cubres.vector_coordinates);
 
         while (window.pollEvent(event))
         {
